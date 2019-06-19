9d7d21bf621b8bcd5ae1b0eb9b12a75fb09f5ec3
hihiboss <junhui820@gmail.com>
2019-06-20 04:07:09 +0900

    feat: implement transaction history view [ISSUE-4]

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 import {Ghost} from '@/types';
+import {Tx} from '@/types/tx';
 
 Vue.use(Vuex);
 
@@ -10,6 +11,30 @@ export const store = new Vuex.Store({
       {gene: 'token ID', evilPoint: 90, angelPoint: 80, name: 'Token Name', level: 2},
       {gene: 'token ID2', evilPoint: 90, angelPoint: 80, name: 'Token Name2', level: 2},
     ] as Ghost[],
+    myTxList: [
+      {
+        timestamp: '19-06-07 14:39:01',
+        txHash: '0x76B4b17F3…',
+        fromNickname: 'Nettie Keller',
+        fromAddress: '',
+        toNickname: 'HATCHOUT1',
+        toAddress: '',
+        purpose: 'bidding',
+        txFee: 0.0003610,
+        status: 'success',
+      },
+      {
+        timestamp: '19-06-07 14:39:01',
+        txHash: '0x76B4b17F3…',
+        fromNickname: 'Julia Howell',
+        fromAddress: '',
+        toNickname: 'HATCHOUT4',
+        toAddress: '',
+        purpose: 'get NFT in game',
+        txFee: 0.0003610,
+        status: 'fail',
+      },
+    ] as Tx[],
   },
   mutations: {},
   actions: {},
---
+++ src/views/TxHistory.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="tx-history-view">
+        <TxHistorySection v-bind:txList="txList"></TxHistorySection>
+    </div>
+</template>
+
+<script lang="ts">
+    import {Component, Vue} from 'vue-property-decorator';
+    import TxHistorySection from '@/components/sections/TxHistorySection.vue';
+    import {Tx} from '@/types/tx';
+
+    @Component({
+        components: {TxHistorySection},
+    })
+    export default class TxHistory extends Vue {
+        private txList: Tx[];
+
+        constructor() {
+            super();
+            this.txList = this.$store.state.myTxList;
+        }
+    }
+</script>
+
+<style scoped lang="scss">
+    .tx-history-view {
+        width: 1784px;
+        height: 1443px;
+        object-fit: contain;
+        background-image: url('~@/assets/tx-history-view-background.png');
+    }
+</style>
