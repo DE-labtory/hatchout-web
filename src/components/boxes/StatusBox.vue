662040b976a506d06e2e682a558e06077f387fb0
hihiboss <junhui820@gmail.com>
2019-07-08 00:33:54 +0900

    feat: implement transaction history view [ISSUE-4]

---
+++ src/components/boxes/StatusBox.vue
@@ -0,0 +1,61 @@
+<template>
+    <div class="status-box">
+        <span v-bind:status="status"
+              :class="{'success': status==='success', 'fail': status==='fail'}">
+            {{status}}
+        </span>
+    </div>
+</template>
+
+<script lang="ts">
+    import {Component, Prop, Vue} from 'vue-property-decorator';
+    import {Tx} from '@/types/tx';
+
+    @Component
+    export default class StatusBox extends Vue {
+        @Prop() private status!: string;
+    }
+</script>
+
+<style scoped lang="scss">
+    .status-box {
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        justify-content: center;
+
+        .success {
+            width: 66px;
+            height: 18px;
+
+            background-color: #48ae3a;
+
+            font-family: Ubuntu;
+            font-size: 16px;
+            font-weight: normal;
+            font-style: normal;
+            font-stretch: normal;
+            line-height: 1;
+            letter-spacing: -0.28px;
+            text-align: center;
+            color: #ffffff;
+        }
+
+        .fail {
+            width: 66px;
+            height: 18px;
+
+            background-color: #fecb00;
+
+            font-family: Ubuntu;
+            font-size: 16px;
+            font-weight: normal;
+            font-style: normal;
+            font-stretch: normal;
+            line-height: 1;
+            letter-spacing: -0.28px;
+            text-align: center;
+            color: #aa4c4c;
+        }
+    }
+</style>
---
+++ src/components/sections/TxHistorySection.vue
@@ -0,0 +1,121 @@
+<template>
+    <div class="tx-history-section">
+        <div class="tx-filter-section" style="width: 1140px; height: 48px;">
+            <div class="tx-list-filters">
+                <div class="sort-method-item" @click="activeSortMethod = 'allTxList'"
+                     v-bind:class="{active: activeSortMethod ==='allTxList' }">
+                    <TxRadioBullet v-bind:isActive="activeSortMethod ==='allTxList'"></TxRadioBullet>
+                    <div class="radio-name">All transaction list</div>
+                </div>
+                <div class="sort-method-item" @click="activeSortMethod = 'myTxList'"
+                     v-bind:class="{active: activeSortMethod ==='myTxList' }">
+                    <TxRadioBullet v-bind:isActive="activeSortMethod ==='myTxList'"></TxRadioBullet>
+                    <div>My transaction list</div>
+                </div>
+            </div>
+            <div class="tx-purpose-filters">
+                <div>Select</div>
+                <div>Purpose</div>
+            </div>
+        </div>
+        <TxTable v-bind:txList="txList"></TxTable>
+    </div>
+</template>
+
+
+<script lang="ts">
+    import {Component, Prop, Vue} from 'vue-property-decorator';
+    import TxTable from '@/components/tables/TxTable.vue';
+    import TxRadioBullet from '@/components/bullets/TxRadioBullet.vue';
+    import {Tx} from '@/types/tx';
+
+    enum MenuType {
+        ALL_TX_LIST = 'allTxList',
+        MY_TX_LIST = 'myTxList',
+    }
+
+    @Component({
+        components: {TxTable, TxRadioBullet},
+    })
+    export default class TxHistorySection extends Vue {
+        @Prop() private txList!: Tx[];
+        private activeSortMethod: MenuType;
+
+        constructor() {
+            super();
+            this.activeSortMethod = MenuType.ALL_TX_LIST;
+        }
+    }
+</script>
+
+<style scoped lang="scss">
+    .tx-history-section {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+
+        .tx-filter-section {
+            display: flex;
+            flex-direction: row;
+            vertical-align: middle;
+            justify-content: space-between;
+
+            .tx-list-filters {
+                width: 430px;
+                height: 48px;
+
+                display: flex;
+                flex-direction: row;
+                vertical-align: middle;
+
+                .sort-method-item {
+                    width: 215px;
+                    height: 48px;
+                    opacity: 0.3;
+                    color: #ffffff;
+
+                    display: flex;
+                    flex-direction: row;
+                    align-items: center;
+                    justify-content: center;
+
+                    font-family: Ubuntu;
+                    font-size: 18px;
+                    font-weight: normal;
+                    font-style: normal;
+                    font-stretch: normal;
+                    line-height: normal;
+                    letter-spacing: -0.3px;
+
+                    .radio-name {
+                        opacity: 1;
+                    }
+                }
+
+                .sort-method-item:hover {
+                    opacity: 1;
+                }
+
+                .active {
+                    opacity: 1;
+                }
+            }
+
+            .tx-purpose-filters {
+                display: flex;
+                flex-direction: row-reverse;
+                vertical-align: middle;
+
+                font-family: Ubuntu;
+                font-size: 16px;
+                font-weight: normal;
+                font-style: normal;
+                font-stretch: normal;
+                line-height: normal;
+                letter-spacing: -0.02px;
+                color: #ffffff;
+            }
+        }
+    }
+</style>
