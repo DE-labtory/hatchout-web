ddf06ea922e8528f4079a990efac7dd4097019f0
junk-sound <wom2277@naver.com>
2019-08-12 22:32:51 +0900

    feat: implement UserApi [ISSUE-14]

--- src/api/user.api.ts
+++ src/api/user.api.ts
@@ -1,11 +1,49 @@
-import { injectable } from 'vue-typescript-inject';
-import {SignInDto} from '@/types';
-import {AxiosInstance} from 'axios';
 
-@injectable()
+import {SignInDto, User} from '@/types';
+import {AxiosResponse} from 'axios';
+import {Inject, Injectable} from 'vue-typedi';
+import {DeleteResultDto} from '@/types/deleteResultDto';
+import {AxiosService} from '@/api/axios/axios.service';
+
+@Injectable()
 export class UserApi {
-  constructor(private readonly axios: AxiosInstance) {}
+
+  private domain = 'users';
+
+  @Inject()
+  private axiosService!: AxiosService;
+
   public async signIn(): Promise<SignInDto> {
     return {} as SignInDto;
   }
+  public async get(id: number): Promise<User> {
+    const axiosInstance = this.axiosService.get();
+    const response: AxiosResponse<User> = await axiosInstance.get<User>(`${this.domain}/${id}`);
+    return response.data;
+  }
+  public async delete(id: number): Promise<DeleteResultDto> {
+    const axiosInstance = this.axiosService.get();
+    const response: AxiosResponse<DeleteResultDto> =
+      await axiosInstance.delete<DeleteResultDto>(`${this.domain}/${id}`);
+    return response.data;
+  }
+  public async increaseLevel(id: number, amount: number): Promise<User> {
+    const axiosInstance = this.axiosService.get();
+    const response: AxiosResponse<User> = await axiosInstance.put<User>
+    (`${this.domain}/${id}/increase-level`, null, {params: {amount}});
+    return response.data;
+  }
+  public async increasePoint(id: number, amount: number): Promise<User> {
+    const axiosInstance = this.axiosService.get();
+    const response: AxiosResponse<User> = await axiosInstance.put<User>
+    (`${this.domain}/${id}/increase-point`, null, { params: {amount}});
+    return response.data;
+  }
+  public async decreasePoint(id: number, amount: number): Promise<User> {
+    const axiosInstance = this.axiosService.get();
+    const response: AxiosResponse<User> = await axiosInstance.put<User>
+    (`${this.domain}/${id}/decrease-point`, null, { params: {amount}});
+    return response.data;
+  }
+
 }
