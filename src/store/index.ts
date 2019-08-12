6a7b8c32dd4bdd13e6b89b8df11b495076a7d577
junk-sound <wom2277@naver.com>
2019-08-12 22:34:29 +0900

    feat: implement UserModule DI [ISSUE-17]

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,1 +1,22 @@
-export * from './store';
+import Vue from 'vue';
+import Vuex from 'vuex';
+
+import { createVuexStore } from 'vuex-simple';
+import {Store} from './store';
+import {Container} from 'vue-typedi';
+import tokens from './tokens';
+
+
+Vue.use(Vuex);
+
+const instance = new Store();
+
+Container.set(tokens.USER_MODULE, instance.userModule);
+
+export const store = createVuexStore(instance, {
+  strict: false,
+  modules: {},
+  plugins: [],
+
+});
+
