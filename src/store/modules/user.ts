86560c6f424f8e95a2acc5af03443a5853039810
junk-sound <wom2277@naver.com>
2019-08-16 23:47:52 +0900

    feat: implement user module [ISSUE-27]

--- src/store/modules/user.ts
+++ src/store/modules/user.ts
@@ -1,10 +1,38 @@
 
 import {Inject, Injectable} from 'vue-typedi';
-import {UserApi} from '@/api';
+import {Getter, Mutation, State} from 'vuex-simple';
+import {User} from '@/types';
+import {UserApi} from '@/api/user/user.api';
+import {AuthApi} from '@/api/auth/auth.api';
+import {UserTranslator} from '@/api/user/user.translator';
+
 
 @Injectable()
 export class UserModule {
 
   @Inject()
   private userApi!: UserApi;
+  @Inject()
+  private authApi!: AuthApi;
+  @Inject()
+  private userTranslator!: UserTranslator;
+
+  @State()
+  private self!: User;
+
+  public async signIn(address: string, message: string, signature: string): Promise<User> {
+    const signInDto = await this.authApi.signIn(address, message, signature);
+    return this.updateSelf(this.userTranslator.translate(signInDto));
+  }
+
+  @Getter()
+  public get getSelf(): User {
+    return this.self;
+  }
+
+  @Mutation()
+  private updateSelf(self: User): User {
+    this.self = self;
+    return this.self;
+  }
 }
