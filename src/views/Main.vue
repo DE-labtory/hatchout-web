fd6996577bedcc00ee2f2ab2bc78ff72e8af67f4
junk-sound <wom2277@naver.com>
2019-08-21 22:55:18 +0900

    impl: change store modules(ghost, tx) beacuse of vuex-simple

--- src/views/Main.vue
+++ src/views/Main.vue
@@ -9,16 +9,19 @@
 import {Vue, Component} from 'vue-property-decorator';
 import MarketPlaceSection from '@/components/sections/MarketPlaceSection.vue';
 import {Ghost} from '../types';
+import {Store} from "@/store/store";
+import {useStore} from "vuex-simple";
 
 @Component({
     components: {MarketPlaceSection},
 })
 export default class Main extends Vue {
     private ghosts: Ghost[];
+    private store: Store = useStore(this.$store);
 
     constructor() {
         super();
-        this.ghosts = this.$store.state.myGhosts;
+        this.ghosts = this.store.ghostModule.getGhosts;
     }
 
 }
