c26d024af9096a1bb45b57c8b58d5c91911f3056
junk-sound <wom2277@naver.com>
2020-03-14 21:36:03 +0900

    feat: sync ghost type with server [ISSUE: 40]

--- src/types/ghost.ts
+++ src/types/ghost.ts
@@ -1,6 +1,10 @@
 export interface Ghost {
   readonly name: string;
-  readonly address: string;
-  readonly point: number;
+  readonly gene: string;
+  readonly tokenId: number;
+  readonly userAddress: string;
   readonly level: number;
+  readonly angelPoint: number;
+  readonly devilPoint: number;
+  readonly createdDate: number;
 }
